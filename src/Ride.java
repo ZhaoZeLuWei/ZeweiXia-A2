fbcbb0105c1aa89024bd2e2c646cf8a5ce877952
Zewei Xia <xiazeweibobby@gmail.com>
2024-12-12 19:11:27 +0800

    Ride class creation
    Finishing the creation of Ride class

--- src/Ride.java
+++ src/Ride.java
@@ -1,3 +1,65 @@
 public class Ride {
-    
+    private String rideName;
+    private boolean isRunning; 
+    private Employee employeeInfo;
+    private int maxSeat;
+
+    public Ride(String RideName, boolean IsRunning, Employee EmployeeInfo, int MaxSeat) {
+        this.rideName = RideName;
+        this.isRunning = IsRunning;
+        this.employeeInfo = EmployeeInfo;
+        this.maxSeat = MaxSeat;
+    }
+
+    public Ride() {
+        this.rideName = "NULL-rideName";
+        this.isRunning = false;
+        this.employeeInfo = new Employee();
+        this.maxSeat = 0;
+    }
+
+    public String getRideName() {
+        return this.rideName;
+    }
+
+    public boolean getIsRunning() {
+        return this.isRunning;
+    }
+
+    public Employee employeeInfo() {
+        return this.employeeInfo;
+    }
+
+    public int getMaxSeat() {
+        return this.maxSeat;
+    }
+
+    public void setRideName(String newRideName) {
+        if(newRideName != null) {
+            this.rideName = newRideName;
+        } else {
+            throw new IllegalArgumentException("RideName cannot be null");
+        }
+    }
+
+    public void setRunning(boolean newRuning) {
+        if(this.isRunning == newRuning) {
+            throw new IllegalArgumentException(String.format("Ride is already %s", newRuning ? "running" : "not running"));
+        }
+        this.isRunning = newRuning;
+    }
+
+    public void setEmployee(Employee newEmployee) {
+        if (newEmployee == null) {
+            throw new IllegalArgumentException("RideName cannot be null");
+        }
+        this.employeeInfo = newEmployee;
+    }
+
+    public void setMaxSeat(int newMaxSeat) {
+        if(newMaxSeat <= 0) {
+            throw new IllegalArgumentException("Set seat number must be greater than 0");
+        }
+        this.maxSeat = newMaxSeat;
+    }
 }
